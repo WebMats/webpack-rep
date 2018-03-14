7c1f3f0e5acd6819a0782c908c2249ce8a6cbeed
OrlandoL10 <lorlando127@gmail.com>
2018-03-14 08:34:36 -0500

    updated common to include css-loaders

--- webpack.common.js
+++ webpack.common.js
@@ -6,14 +6,25 @@ module.exports = {
 	entry: {
 			app: './src/index.js'
 			},
-	plugins: [
-			new CleanWebpackPlugin(['dist']),
-			new HtmlWebpackPlugin({
-								title: 'Production'
-								})
-			],
 	output: {
 			filename: '[name].bundle.js',
-			path: path.resolve(__dirname, 'dist')
+			path: path.resolve(__dirname, 'build')
+			},
+	plugins: [
+			new CleanWebpackPlugin(['build']),
+			new HtmlWebpackPlugin({
+								title: 'Production'
+									})
+			],
+	module: {
+			rules: [
+						{
+							test: /\.css$/,
+							use: [
+								'style-loader',
+								'css-loader'
+								]
+						}
+					]
 			}
 };
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,31 +1,15 @@
 const merge = require('webpack-merge');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const common = require('./webpack.common.js');
 const webpack = require('webpack');
 
 module.exports = merge(common, {
 	devtool: 'source-map',
 	plugins: [
-			new UglifyJSPlugin(),
 			new webpack.DefinePlugin({
-								'process.env.NODE_ENV': JSON.stringify('production')
+							'process.env.NODE_ENV': JSON.stringify('production')
 									})
 			],
 	module: {
-			rules: [
-				{
-				test: /\.css$/,
-				use: [
-					'style-loader',
-					'css-loader'
-					]
-				},
-				{
-				test: /\.(png|svg|jpg|gif)$/,
-				use: [
-					'file=loader'
-					]
-				}
-				]
+			
 			}
 });
